95ceb81ee7749cd63f9ca8322534b5d94d53fc36
鎌田大輝 <zeal23300159@fsgcl.jp>
2025-03-24 09:32:27 +0900

    クラス修正

--- Project/GameSources/EnemyRouteSearch.cpp
+++ Project/GameSources/EnemyRouteSearch.cpp
@@ -8,5 +8,35 @@
 #include "EnemyRouteSearch.h"
 
 namespace basecross {
+	Navigate::Navigate(const shared_ptr<Stage>& stage, vector<shared_ptr<RootPointer>> points):
+		GameObject(stage),
+		m_Points(points)
+	{
+	}
+	Navigate::~Navigate()
+	{
+	}
+	void Navigate::OnCreate()
+	{
+	}
+	void Navigate::OnUpdate()
+	{
+	}
+	void Navigate::SetTargetPosition(Vec3 newTargetPosition)
+	{
+		m_TargetPosition = newTargetPosition;
+		AStarAlgorithm();
+	}
+	void Navigate::AStarAlgorithm()
+	{
+	}
+	void Navigate::OpenCell(Vec3 index)
+	{
+		
+	}
+	bool Navigate::UpdateDistaince(Vec3 index)
+	{
+       return false;
+    }
 }
 //end basecross
