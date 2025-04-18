c3aef5d7ec8676c3a981370e31f4514724f2100e
SatouRyuuma <zeal23300082@fsgcl.jp>
2025-04-18 12:17:20 +0900

    menuで設定できるように

--- Project/GameSources/Menu.cpp
+++ Project/GameSources/Menu.cpp
@@ -37,9 +37,17 @@ namespace basecross {
 			obj->SetDrawActive(false);
 			//obj->SetUpdateActive(false);
 		}
+		EffeckPause();
 		ButtonManager::instance->Close(m_GroupName);
 		SoundManager::Instance().PauseBGM(false);
 		m_IsOpen = false;
+	}
+
+	void Menu::EffeckPause()
+	{
+		auto GetStage = GetTypeStage<GameStage>();
+		auto Effect = GetStage->GetCreateEffect();
+		Effect->SetEffectPause(false);
 	}
 
 	void PauseMenu::OnCreate() {
@@ -63,7 +71,6 @@ namespace basecross {
 			[](shared_ptr<ObjectInterface> object) {
 				auto menu = static_pointer_cast<Menu>(object);
 				menu->Close();
-				//auto Stage = static_pointer_cast<GameStage>(m_Stage);
 			});
 		AddButton(L"POSE_SOUND", L"POSE_SOUND_SELECTED", Vec3(0.0f, -150.0f, 0.0f), Vec2(200, 50), menu,
 			[](shared_ptr<ObjectInterface> object) {
