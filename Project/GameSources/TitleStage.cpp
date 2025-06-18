718da66f06ed4c8aef31faf67b1eef3025904c1e
鎌田大輝 <zeal23300159@fsgcl.jp>
2025-06-18 15:01:32 +0900

    タイトル演出

--- Project/GameSources/TitleStage.cpp
+++ Project/GameSources/TitleStage.cpp
@@ -12,8 +12,8 @@ namespace basecross {
 	//	ゲームステージクラス実体
 	//--------------------------------------------------------------------------------------
 	void TitleStage::CreateViewLight() {
-		const Vec3 eye(0.0f, 5.0f, -5.0f);
-		const Vec3 at(0.0f);
+		const Vec3 eye(0.0f, 2.5f, -5.0f);
+		const Vec3 at(0.0f, 2.5f, 0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ビューのカメラの設定
 		auto PtrCamera = ObjectFactory::Create<Camera>();
@@ -31,20 +31,28 @@ namespace basecross {
 		auto mediaPath = app->GetDataDirWString();
 		wstring uiPath = mediaPath + L"UI/";
 		wstring texPath = mediaPath + L"Textures/";
+		wstring effectPath = mediaPath + L"Effekt/";
 
 		app->RegisterTexture(L"TITLESPRITE", uiPath + L"Title_Rogo.png");
 		app->RegisterTexture(L"STRATA", uiPath + L"StartA.png");
 		app->RegisterTexture(L"FADE", uiPath + L"TitelFade.png");
 		app->RegisterTexture(L"BACKGROUND", texPath + L"TitleBackGround.png");
+		m_Effect = AddGameObject<EffectManeger>();
+		m_Effect->SetDrawLayer(4);
+		m_Effect->RegisterResource(L"Panchi", effectPath + L"panchi.efk");
+
 	}
 
 	void TitleStage::CreateTitle() {
-		auto backGround = AddGameObject<Sprite>(L"BACKGROUND", Vec3(0.0f, 0.0f, 0.0f), Vec2(1280.0f, 800.0f), true);
-		auto titleSprite = AddGameObject<Sprite>(L"TITLESPRITE", Vec3(0.0f, 150.0f, 0.0f), Vec2(1500.0f, 1300.0f),true);
-		auto stratASprite = AddGameObject<Sprite>(L"STRATA", Vec3(0.0f, -250.0f, 0.0f), Vec2(400.0f, 160.0f),true);
+		auto backGround = AddGameObject<Sprite>(L"BACKGROUND", Vec3(0.0f, 0.0f, 5.0f), Vec2(1280.0f, 800.0f), true);
+		m_Title = AddGameObject<Sprite>(L"TITLESPRITE", Vec3(0.0f, 150.0f, 0.0f), Vec2(1500.0f, 1300.0f),true);
+		m_Title->SetDrawLayer(6);
+		m_Start = AddGameObject<Sprite>(L"STRATA", Vec3(0.0f, -250.0f, 0.0f), Vec2(400.0f, 160.0f),true);
+		m_Start->SetDrawLayer(6);
 		auto fadeSprite = AddGameObject<Sprite>(L"FADE", Vec3(0.0f, 0.0f, 0.0f), Vec2(1480.0f, 880.0f), true);
+		m_Titlemodel = AddGameObject<TirleStageModel>();
 		//点滅設定
-		stratASprite->AddComponent<SpriteFlash>(0.8f);
+		m_Start->AddComponent<SpriteFlash>(0.8f);
 		m_Fade = fadeSprite->AddComponent<SpriteFade>(1.0f);
 		m_Fade->FadeOut();
 		m_Fade->Stop();
@@ -68,20 +76,90 @@ namespace basecross {
 		m_InputHandler.PushHandle(GetThis<TitleStage>());
 		auto& app = App::GetApp();
 		auto& cntlVec = App::GetApp()->GetInputDevice().GetControlerVec()[0];
+		if (m_EffectPos != Vec3())
+		{
+			m_EffectPos = m_EffectPos + Vec3(0, 0, -0.1f);
+			m_Effect->SetLocation(m_Handle, m_EffectPos);
+		}
+		if (m_Titlemodel->GetEndFlag())
+		{
+			m_Fade->Play();
+			m_Effect->SetEffectPause(true);
+		}
 		if (m_Fade->IsFinish())
 		{
 			PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToSelectStage");
-
 		}
-
 	}
 
 	void TitleStage::OnPushA() {
-		//ボタンを押されたらtrue
-		//PostEvent(0.0f, GetThis<ObjectInterface>(), App::GetApp()->GetScene<Scene>(), L"ToGameStageKamata");
-		m_Fade->Play();
+		auto camera = GetView()->GetTargetCamera();
+		auto forward = camera->GetEye() - camera->GetAt();
+		float rotate = atan2f(forward.x, forward.z);
+		m_EffectPos = Vec3(0.01f, 2.05f, -2.0f);
+		m_Effect->PlayEffect(m_Handle, L"Panchi", m_EffectPos, 0.0f);
+		m_Effect->SetRotation(m_Handle, Vec3(0, 1, 0), rotate);
+		m_Effect->SetEffectSpeed(m_Handle, 0.1f);
+		m_Titlemodel->SetFlag(true);
+		m_Start->SetDrawActive(false);
+		m_Title->SetDrawActive(false);
 		SoundManager::Instance().PlaySE(L"SE_HIT_ENEMY");
+	}
+
+	void TirleStageModel::OnCreate() {
+		Object::OnCreate();
+
+		m_Transform = GetComponent<Transform>();
+		SetPosition(Vec3(0));
+		SetScale(Vec3(1));
+		SetRotation(Vec3(0));
+
+
+		auto ptrDraw = AddComponent<BcPNTBoneModelDraw>();
+		Mat4x4 meshMat;
+		meshMat.affineTransformation(
+			Vec3(1.0f), //(.1f, .1f, .1f),
+			Vec3(0.0f, 0.0f, 0.0f),
+			Vec3(0.0f, 0.0, 0.0f),
+			Vec3(0.0f, 0.25f, 0.0f)
+		);
+		ptrDraw->SetMeshResource(L"TITLEBREAK");
+		ptrDraw->SetTextureResource(L"BACKTIRLEBRAKE");
+		ptrDraw->SetMeshToTransformMatrix(meshMat);
+		ptrDraw->SetBlendState(BlendState::Opaque);
+		//ptrDraw->SetOwnShadowActive(true);
+		SetDrawLayer(5);
+		ptrDraw->AddAnimation(L"TITLEBREAK", 0.0f, 120.0f, false, 240.0f);
+		ptrDraw->AddAnimation(L"TITLEFALL", 60.0f, 120.0f, false, 60.0f);
+		SetAnim(L"TITLEBREAK");
 
 	}
 
+	void TirleStageModel::OnUpdate() {
+
+		Vec3 defUp = Vec3(0, 1, 0);
+		auto camera = OnGetDrawCamera();
+		Vec3 eye = camera->GetEye();
+		Vec3 at = camera->GetAt();
+
+		Vec3 temp = at - eye;
+		Vec2 tempVec2(temp.x, temp.z);
+		if (length(tempVec2) < 0.1f) {
+			defUp = bsm::Vec3(0, 0, 1.0f);
+		}
+		temp.normalize();
+		Mat4x4 rotMatrix = static_cast<Mat4x4>(XMMatrixLookAtLH(Vec3(0, 0, 0), temp, defUp));
+		rotMatrix = inverse(rotMatrix);
+		Quat Qt = rotMatrix.quatInMatrix();
+		Qt.normalize();
+
+		GetComponent<Transform>()->SetQuaternion(Qt);
+		auto ptrDraw = GetComponent<BcPNTBoneModelDraw>();
+		auto elapsedTime = App::GetApp()->GetElapsedTime();
+		if (m_Flag)
+		{
+			ptrDraw->UpdateAnimation(elapsedTime);
+			m_EndFlag = ptrDraw->IsTargetAnimeEnd();
+		}
+	}
 }
