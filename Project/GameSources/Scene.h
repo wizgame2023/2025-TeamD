cc7f6e057f140731481dded4a46d6728094ab0f4
鎌田大輝 <zeal23300159@fsgcl.jp>
2025-06-24 15:56:44 +0900

    攻撃のインターバル減少

--- Project/GameSources/Scene.h
+++ Project/GameSources/Scene.h
@@ -29,7 +29,7 @@ namespace basecross{
 		Scene() :SceneBase(){
 			m_StageFile = {
 				{{L"Debugstage.csv",false},{L"testStage_Y_normal.csv",false},{L"testStage_Y_Hard.csv",false}},
-				{{L"testStage_Y_Easy.csv",false},{L"testStage_Y_normal.csv",false},{L"testStage_Y_Hard.csv",false}},
+				{{L"testStage01.csv",false},{L"testStage02.csv",false},{L"testStage03.csv",false}},
 				{{L"testStage_Y_Easy.csv",false},{L"testStage_Y_normal.csv",false},{L"testStage_Y_Hard.csv",false}}
 			};
 		}
