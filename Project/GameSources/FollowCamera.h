ab43ff5f4c9a4bc03207d8dcc0ca4eb689934310
鎌田大輝 <zeal23300159@fsgcl.jp>
2025-07-04 09:30:56 +0900

    コード整理

--- Project/GameSources/FollowCamera.h
+++ Project/GameSources/FollowCamera.h
@@ -59,10 +59,8 @@ namespace basecross {
 		float m_Magnitude;        // 最大振幅（単位：画素やワールド単位）
 		float m_CurrntTime;
 		shared_ptr<Stage>m_Stage;
-		float m_Pitch;   // 上下角度
-		float m_MouseSensitivityX;
+		float m_MouseSensitivityX; // マウス感度（X軸）
 		POINT m_CenterPt;             // 画面中央（スクリーン座標）
-		bool  m_UseMouse = true;   // マウス制御を有効化
 
 		int m_Width;
 		int m_Height;
