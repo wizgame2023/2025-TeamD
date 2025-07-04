a3a41333b4e73b868795bef9d764c6291c0294a9
MSI\moriy <zeal23300082@fsgcl.jp>
2025-07-04 14:56:18 +0900

    新しいステージ（難易度追加）

--- Project/GameSources/Scene.h
+++ Project/GameSources/Scene.h
@@ -30,7 +30,7 @@ namespace basecross{
 			m_StageFile = {
 				{{L"testStage_Y_Easy.csv",false},{L"testStage_Y_normal.csv",false},{L"testStage_Y_Hard.csv",false}},
 				{{L"TestMap_Easy.csv",false},{L"TestMap_Normal.csv",false},{L"TestMap_Hard.csv",false}},
-				{{L"testStage_K_Easy.csv",false},{L"testStage_Y_normal.csv",false},{L"testStage_Y_Hard.csv",false}}
+				{{L"Map_S_Easy.csv",false},{L"Map_S_Normal.csv",false},{L"Map_S_Hard.csv",false}}
 			};
 		}
 		//--------------------------------------------------------------------------------------
